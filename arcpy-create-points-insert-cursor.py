ae2c174f2083f1b3fa91ec39b26f26528be7fc7f
William Lorand <bill@milemarker3.org>
2016-03-08 13:10:55 -0800

    initial commit

---
+++ arcpy-create-points-insert-cursor.py
@@ -0,0 +1,59 @@
+# filename:  arcpy-create-points-insert-cursor.py
+# purpose: read in a txtfile (csv) via Python I/O
+# and use the ArcPy module's Insert Cursor to put the data into a shapefile (.shp) 
+
+# imports and set local vars
+import arcpy, sys, os.path
+from arcpy import env
+from arcpy import management as DM
+
+scriptdir = os.path.dirname(sys.argv[0])
+dataWS = "data"
+ptfeatname = "sample_pts_from_csv_3.shp"
+dir = scriptdir + "/" + dataWS
+ptfeatpath = dir + "/" + ptfeatname
+
+# env settings
+env.workspace = dir
+
+# create dir if it doesn't exist
+if not arcpy.Exists(dir):
+    DM.CreateFolder(scriptdir, dataWS)
+
+# grab spatial reference object from existing data
+sr = arcpy.Describe(r"F:\prog\marbles\samples.shp").spatialReference
+
+# Read in some sample pts from a csv
+inputFile = r"F:\prog\Marbles\samples.csv" 
+textin = open(inputFile, "r")
+
+# Create an empty Shapefile featureclass (fc)
+ptfeatures = DM.CreateFeatureclass(dir, ptfeatname, "POINT", "", "", "", sr)
+
+# Add "SAMPLES_ID" and "CATOT" Fields to your fc
+DM.AddField(ptfeatures, "SAMPLES_ID", "LONG")
+DM.AddField(ptfeatures, "CATOT", "DOUBLE")
+
+# Create an Insert Cursor and add point data to your fc
+cur = arcpy.InsertCursor(ptfeatures)
+firstRow = True
+for txtrow in textin:
+    if not firstRow:
+        ptfeat = cur.newRow()
+        values = txtrow.split(",")
+        # build geometry from x,y in the csv
+        ptobj = arcpy.Point(float(values[2]), float(values[3]))
+        ptfeat.shape = ptobj
+        # populate the other fields
+        ptfeat.SAMPLES_ID = int(float(values[0]))
+        ptfeat.CATOT = float(values[1])
+        # actually insert the row
+        cur.insertRow(ptfeat)
+    firstRow = False
+
+# Add X,Y Data to your fc (this must get its data from the Point obj)
+DM.AddXY(ptfeatures)
+
+# del objects and close text files for garbage collection
+textin.close()
+del cur, sr, ptfeat
---
+++ arcpy-geo-raster-random-points.py
@@ -0,0 +1,54 @@
+# filename: arcpy-geo-raster-random-points.py
+# purpose: Create 100 random points within the extent of an elevation raster 
+#          and then write out a text file of point coordinates (x,y) 
+
+# Imports
+import arcpy, sys, os.path, random
+from arcpy import env
+
+# Set file and workspace settings
+scriptdir = os.path.dirname(sys.argv[0])
+datadir = scriptdir + "\ExamData\HMB"
+env.workspace = datadir
+# print "workspace: " + env.workspace
+
+# Get the extent values from the elev raster
+descRas = arcpy.Describe("elev")
+ext = descRas.extent
+print "\nXMin: " + str(ext.XMin) + "\nXMax: " + str(ext.XMax) + "\nYMin: " + str(ext.YMin) + "\nYMax: " + str(ext.YMax)
+
+# Get a random x-value within the extent
+# print type(ext.XMin) # <type 'float'>
+xRange = ext.XMax - ext.XMin
+# print xRange, type(xRange)
+# oneRandX = xRange * random.random() + ext.XMin
+# print oneRandX
+
+# Get a random y-value within the extent
+# print type(ext.YMin) # <type 'float'>
+yRange = ext.YMax - ext.YMin
+# print yRange, type(yRange)
+# oneRandY = yRange * random.random() + ext.YMin
+# print oneRandY
+
+# Create some empty lists for X and Y and populate them with 100 random values in the extent of the raster 
+randXList = []
+randYList = [] 
+for i in range(0, 100):
+    randXValue = xRange * random.random() + ext.XMin
+    randXList.append(int(randXValue))
+    randYValue = yRange * random.random() + ext.YMin
+    randYList.append(int(randYValue))
+print randXList
+print randYList
+
+# open a text file to write out the random X, Y as point coordinates (space-delimited)
+# (opening with a write handle "w" should create the file if it doesn't exist)
+# todo: enhance by deleting the file if it already exists (not coded here)
+pointfilepath = datadir + "\\randpts.txt"
+out_randpts = open(pointfilepath, "w")
+out_randpts.write("X Y \n") # header line
+for i in range(len(randXList)):
+    out_randpts.write( str(randXList[i]) + " " + str(randYList[i]) + "\n")
+out_randpts.close()
+
